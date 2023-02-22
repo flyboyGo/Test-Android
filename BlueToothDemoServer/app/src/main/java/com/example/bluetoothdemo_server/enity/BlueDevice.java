5b7a7b6f37e98a3b180bf09109767aa17d9d3e01
flyboy <3183615461@qq.com>
2023-02-22 14:31:11 +0800

    BleServer

---
+++ BlueToothDemoServer/app/src/main/java/com/example/bluetoothdemo_server/enity/BlueDevice.java
@@ -0,0 +1,13 @@
+package com.example.bluetoothdemo_server.enity;
+
+public class BlueDevice {
+    public String name;
+    public String address;
+    public int state;
+
+    public BlueDevice(String name, String address, int state) {
+        this.name = name;
+        this.address = address;
+        this.state = state;
+    }
+}
