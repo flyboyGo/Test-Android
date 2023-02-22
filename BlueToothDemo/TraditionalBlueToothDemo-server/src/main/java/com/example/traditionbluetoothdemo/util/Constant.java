5be3e89442fd242ee484fbb4a8ae6081a1d96dbd
flyboy <3183615461@qq.com>
2023-02-22 14:31:37 +0800

    Ble

---
+++ BlueToothDemo/TraditionalBlueToothDemo-server/src/main/java/com/example/traditionbluetoothdemo/util/Constant.java
@@ -0,0 +1,7 @@
+package com.example.traditionbluetoothdemo.util;
+
+import java.util.UUID;
+
+public class Constant {
+    public static UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+}
